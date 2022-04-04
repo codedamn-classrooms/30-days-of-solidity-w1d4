f4b4b4cf47afc242bb36854d3d92d78efc296551
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-04-04 16:57:35 +0530

    Update and rename day4.sol to day5.sol

--- contracts/day5.sol
+++ contracts/day5.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
-contract day4 {
+contract day5 {
     function find(int256 a) public pure returns (int256) {
         return a % 3;
     }
